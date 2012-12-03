00c052fe7adc30084194dee9025d8156d58f85ff
Robert Ferney <capnregex@gmail.com>
2012-12-02 18:35:24 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageArchive::Application.config.secret_token = '55e98ebbb9bd2f904c3967139066d19deeb1f184f9d788a2ade2adf9e03b42c808aee2ece7d85efa5ea55f7286bc3ce7737353859ccbdbdd9b788c80bb17f02f'
